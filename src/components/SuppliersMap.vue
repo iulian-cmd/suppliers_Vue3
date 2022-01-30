cb2c7a5bb6a6393759ea4c5caccc4893fa819b65
iulian-cmd <flysteelbird@gmail.com>
2022-01-30 23:47:48 +0100

    center map to chambery and add marker

--- src/components/SuppliersMap.vue
+++ src/components/SuppliersMap.vue
@@ -1,12 +1,15 @@
 <template>
-<div>
- <div id="map">  </div>
+
+ <div class="container-fluid my-5 border border-3">  
+
+ <div id="map" class="my-5">  
  <Markers 
       v-for="marker in markers"
       :key="marker.id"
       :longitude="marker.latitude"
       :latitude="marker.longitude"
  />
+</div>
 </div>
 </template>
 
@@ -36,8 +39,8 @@ export default {
   setup() {
     let mymap;
     onMounted(() => {
-      mymap = leaflet.map('map').setView([51.505, -0.09], 13);
-      leaflet.marker([51.5, -0.09]).addTo(mymap);
+      mymap = leaflet.map('map').setView([45.64080539261331, 5.874018113832485], 14);
+      leaflet.marker([45.64080539261331, 5.874018113832485]).addTo(mymap);
 
       leaflet.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiZmx5c3RlZWwiLCJhIjoiY2t2YWhkZjByMmJ6ZDJybHVvcGk5dGVkNiJ9.av_gL3YCSSYoJ-_fx1yYJw', {
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright%22%3EOpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/%22%3EMapbox</a>',
